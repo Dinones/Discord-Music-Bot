2c8db77884e84c1d6f1f964a49039378b60441bd
Dinones <109595182+Dinones@users.noreply.github.com>
2026-04-01 15:01:03 +0000

    [Terraform] Before adding the S3 bucket for the shared Terraform state

--- Cloud/main.tf
+++ Cloud/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
     region = var.aws_region
 }
 
-resource "aws_secretsmanager_secret" "app" {
+resource "aws_secretsmanager_secret" "discord_bot_secret" {
     name                    = var.secret_name
     description             = "Discord Music Bot Token"
     recovery_window_in_days = 7
@@ -22,4 +22,44 @@ resource "aws_secretsmanager_secret" "app" {
         Project = var.project_name
         Managed = "Terraform"
     }
+}
+
+data "aws_iam_policy_document" "secret_reader" {
+    statement {
+        sid    = "ReadOnlyBotSecret"
+        effect = "Allow"
+
+        actions = [
+            "secretsmanager:GetSecretValue"
+        ]
+
+        resources = [
+            aws_secretsmanager_secret.discord_bot_secret.arn
+        ]
+    }
+}
+
+resource "aws_iam_user" "secret_reader" {
+    count = var.create_secret_reader_user ? 1 : 0
+
+    name = var.secret_reader_user_name
+
+    tags = {
+        Project = var.project_name
+        Managed = "Terraform"
+    }
+}
+
+resource "aws_iam_user_policy" "secret_reader" {
+    count = var.create_secret_reader_user ? 1 : 0
+
+    name   = "${var.secret_reader_user_name}-read-secret"
+    user   = aws_iam_user.secret_reader[0].name
+    policy = data.aws_iam_policy_document.secret_reader.json
+}
+
+resource "aws_iam_access_key" "secret_reader" {
+    count = var.create_secret_reader_user && var.create_secret_reader_access_key ? 1 : 0
+
+    user = aws_iam_user.secret_reader[0].name
 }
